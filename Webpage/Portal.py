60ab4fe0b5946e969a50f5a8b6afaadd6472d13a
Mayank Bansal <162439698+PyCoder23@users.noreply.github.com>
2024-03-09 10:57:58 +0530

    Update Portal.py

--- Webpage/Portal.py
+++ Webpage/Portal.py
@@ -1,3 +1,6 @@
+# It might be possible that you encounter error(s) while running this because of mysql/xampp or library(s) not installed.
+# But this will surely help you in your programs. All the BEST!
+
 from tkinter import *
 import tkinter as tk
 from tkinter import ttk
